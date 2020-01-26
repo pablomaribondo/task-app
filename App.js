cd2827853c5f572a92082d4d74881c579fbb1866
Pablo Maribondo <pablomgvieira@gmail.com>
2020-01-26 14:58:43 -0300

    Commit inicial

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Task'
+          style={styles.input}
+        />
+        <Button title="Add" />
+      </View>
+      <View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input : {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
+});
